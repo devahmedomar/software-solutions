805602562e90f250ec151423fc6e8ebe214c0b05
devahmedomar <dev.a7med.omar@gmail.com>
2024-04-01 02:19:47 +0200

    latests

--- src/app/modules/landing/components/team/team.component.ts
+++ src/app/modules/landing/components/team/team.component.ts
@@ -11,18 +11,18 @@ export class TeamComponent implements OnInit {
         Aos.init({startEvent: 'scroll'})
       }
   slides = [
-    {img: "../../../../../assets/images/team/1.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
-    {img: "../../../../../assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/1.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/3.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
+    {img: "assets/images/team/2.png",name:"Ahmed Omar",title:"Frontend Web Developer"},
 
   ];
   slideConfig = {"slidesToShow": 3, "slidesToScroll": 3,dots:true,
